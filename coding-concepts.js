ecd3b789dc0a464ba85032bebd50a345e364e4d8
SunkissedQueen <baxtercm@outlook.com>
2021-09-18 22:28:51 -0700

    Just need to research interview questions

--- coding-concepts.js
+++ coding-concepts.js
@@ -8,10 +8,10 @@
 // --------------------1) What will this log?
 
 var cohort = "Delta 2021"
-// console.log(cohort.split(""))
+ console.log(cohort.split(""))
 
 // a) Your answer: ['D', 'e','l', 't', 'a', '2', '0', '2', '1']
-// b) Verify and explain: The split() method turns a string into an array. Each character in the string become separate characters within an array.
+// b) Verify and explain: Output ----> ['D', 'e', 'l', 't', 'a', ' ', '2', '0', '2', '1'] My favorite method! My brain always try to find a way to incorporate this builtin method. The split() method is helpful with turning a string into an array. This method will not permanently change the original array. The output of the characters within the array is impacted by if spaces or quotation marks are within the parenthesis. In the case of .split(""), string will become an array of letters and numbers separated by spaces and commas.
 
 
 // --------------------2) What will this log?
@@ -19,28 +19,28 @@ var cohort = "Delta 2021"
 const greeter = (name) => {
   `Hello, ${name}!`
 }
-// console.log(greeter("LEARN Student"))
+ console.log(greeter("LEARN Student"))
 
-// a) Your answer: Hello, LEARN Student
-// b) Verify and explain: String interpolation allows a value to be included within a sentence. This value will be received outside of the function.
+// a) Your answer: Hello, LEARN Student!
+// b) Verify and explain: Output ----> undefined. Oh, the trickery! Because a function is indicated, a return method needs to be included or undefined will be the output. Return holds a description of the output of executed code (function).
 
 
 // --------------------3) What will this log?
 
 var multBy2 = [4, 5, 6, 7, 8].map(value => value * 2)
-// console.log(multBy2)
+ console.log(multBy2)
 
 // a) Your answer:[8, 10, 12, 14, 16]
-// b) Verify and explain: .map() method allows a function to be iterated across an array.
+// b) Verify and explain: Output ----> [8, 10, 12, 14, 16]  .map() method allows a function to be iterated across an array. The output will be the same length as the original array and will be updated with the executed code of the function. The updated array on this problem multiplied each value of the previous array by two.
 
 
 // --------------------4) What will this log?
 
 var oddsOnly = [11, 12, 13, 14, 15].filter(value => value % 2 === 0)
-// console.log(oddsOnly)
+ console.log(oddsOnly)
 
 // a) Your answer:[12, 14]
-// b) Verify and explain: The function is requesting that the even numbers be displayed. To get odd value%2 !== 0.
+// b) Verify and explain: Output ----> [12, 14] The function is requesting that the even numbers be displayed. I would recommend that the variable be renamed to correlate to the expected output or the modolu be changed to produce odd numbers only (value%2 !== 0)) as indicated by the variable declaration...unless the goal is to irritate/prank the user then nothing should be changed. 
 
 
 // --------------------5) What will this log?
@@ -56,4 +56,4 @@ var learnStudent = new Learn("George")
  console.log(learnStudent)
 
 // a) Your answer: class Learn, student: "George", cohort: "Delta", year: 2021
-// b) Verify and explain: Output ----> Learn {student: 'George', cohort: 'Delta', year: 2021} A class is a
+// b) Verify and explain: Output ----> Learn {student: 'George', cohort: 'Delta', year: 2021} A class is the building block of an object. The class Learn can accept three parameters---student, cohort, and year. Hard values were assigned to cohort and year. When the new information ("George") is introduced, it goes to the student parameter. The output of this class is an object entitled Learn that contains keys and values encapsulated with curly braces.
--- interview-questions.md
+++ interview-questions.md
@@ -8,7 +8,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is iteration?
 
-  Your answer:
+  Your answer: Iteration is any method or process that allows a function or code to be executed throughout the entire length of a string, array, or object. For instance, for loop or .map() are ways to iterate across a parameter. Be aware of the datatype because each only respond to a certain style of iteration.
 
   Researched answer:
 
@@ -16,7 +16,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 2. The JavaScript built in method .map() takes predefined parameters. What are they? Which are required and which are optional?
 
-  Your answer:
+  Your answer: .map takes value, index, an array as predefined parameters. All are optional to use; however, one needs to be designated. (Just had an epiphany about errors I have received using .map() before.)
 
   Researched answer:
 
@@ -24,7 +24,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 3. What is object destructuring?
 
-  Your answer:
+  Your answer: Object destructuring is a method that allows keys and values in an object to be called upon by their value or index.
 
   Researched answer:
 
@@ -32,7 +32,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 4. What is the difference between a function and a method?
 
-  Your answer:
+  Your answer: The difference between a function and a method!!! Well, a function is the executable code that uses a series of methods and variables to perform an action and produce an output based on information introduced externally. A method is the structure or key words that help do the heavy lifting in the code. Methods allow the code to be easier to read and less wordy. This feature is wonderful when a person is troubleshooting errors or trying to update a code block.
 
   Researched answer:
 
@@ -40,7 +40,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 5. What is the difference between a class and an object?
 
-  Your answer:
+  Your answer: I like to think of a grocery store when thinking of a class and an object. The class is like the grocery store, filled with various data that can be used to build a hearty meal (object). The object can built pulled from the placeholders within various classes.
 
   Researched answer:
 
@@ -48,7 +48,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 6. STRETCH: What is hoisting in JavaScript?
 
-  Your answer:
+  Your answer: I am not familiar with the term in the Javascript sense. However, I have use physical hoists. Based on that experience, I am assuming that hoisting in JavaScript relates to a process or method that supports transporting or moving parameters.
 
   Researched answer:
 
